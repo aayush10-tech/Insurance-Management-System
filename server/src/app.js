6a81f9b670b883b8457654b7f0a375012ef88b65
Aayush Mahadik <mahadikaayush10@gmail.com>
2026-07-20 19:00:02 +0530

    feat: add async handler and global error middleware

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import customerRoutes from "./routes/customer.routes.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/customers", customerRoutes);
+app.use(errorMiddleware);
 
 export default app;
---
+++ server/src/middleware/error.middleware.js
@@ -0,0 +1,14 @@
+import ApiResponse from "../utils/apiResponse.js";
+
+const errorMiddleware = (err, req, res, next) => {
+  console.error(err);
+
+  return res.status(err.statusCode || 500).json(
+    new ApiResponse(
+      err.statusCode || 500,
+      err.message || "Internal Server Error"
+    )
+  );
+};
+
+export default errorMiddleware;
